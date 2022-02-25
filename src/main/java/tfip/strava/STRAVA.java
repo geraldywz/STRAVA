e91f547a05e63fd0e9e7234152a0a7b25187097b
Gerald Yeo <ywz.gerald@gmail.com>
2022-02-25 15:06:28 +0800

    Intitial Commit.

---
+++ src/main/java/tfip/strava/STRAVA.java
@@ -0,0 +1,13 @@
+package tfip.strava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class STRAVA {
+
+	public static void main(String[] args) {
+		SpringApplication.run(STRAVA.class, args);
+	}
+
+}
---
+++ src/main/java/tfip/strava/util/Constants.java
@@ -0,0 +1,17 @@
+package tfip.strava.util;
+
+public class Constants {
+
+    public static final String URL_OPENLIBRARY = "http://openlibrary.org/";
+
+    public static final String ENV_REDIS_PASSWORD = "REDIS_PASSWORD";
+
+    public static String KEY_REDISPASSWORD;
+
+    public static final String BEAN_LIBRARY_SERVICE = "FETCH_BOOK_SERVICE";
+    public static final String BEAN_BOOK_SERVICE = "BOOK_SERVICE";
+    public static final String BEAN_BOOK_CACHE = "BOOK_CACHE";
+
+    public static final String DEFAULT_PORT = "8080";
+
+}
