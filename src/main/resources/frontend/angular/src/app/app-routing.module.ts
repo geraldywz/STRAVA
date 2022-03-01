7651a9e5654d60e2d11588d4a91d00753cb80baa
Gerald Yeo <ywz.gerald@gmail.com>
2022-03-01 11:04:44 +0800

    Initial implementation of routing.

--- src/main/resources/frontend/angular/src/app/app-routing.module.ts
+++ src/main/resources/frontend/angular/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
+import { MapComponent } from './components/map.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MapComponent },
+  { path: '**', component: MapComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
