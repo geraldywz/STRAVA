d0f252cae19a9bdea8ae84bb28e060115116f87d
Gerald Yeo <ywz.gerald@gmail.com>
2022-03-02 11:16:59 +0800

    Prepping to implement Directions retrieval in SB.

--- src/main/java/tfip/strava/util/Constants.java
+++ src/main/java/tfip/strava/util/Constants.java
@@ -2,6 +2,17 @@ package tfip.strava.util;
 
 public class Constants {
 
+    // API Related
+
+    public static final String API_ROUTE_ENDPOINT = "/api/route";
+    public static final String API_MAP_ENDPOINT = "/api/map";
+
+    // GMAPS Specific
+
+    
+
+    // MARKED FOR DEPRECATION
+
     public static final String URL_OPENLIBRARY = "http://openlibrary.org/";
 
     public static final String ENV_REDIS_PASSWORD = "REDIS_PASSWORD";
