d00129be1aa3038962982ffe4adaeab4fff4b47e
Gerald Yeo <ywz.gerald@gmail.com>
2022-03-09 23:29:22 +0800

    Removed DB credentials from public view.

--- src/main/java/tfip/strava/STRAVA.java
+++ src/main/java/tfip/strava/STRAVA.java
@@ -1,5 +1,7 @@
 package tfip.strava;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class STRAVA {
 
 	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(SpringBootApplication.class);
+		logger.info("SYSENV >>>>> " + System.getenv("DO_USER"));
 		SpringApplication.run(STRAVA.class, args);
 	}
 
